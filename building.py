aa04c7df81eaa97e1701731bda95a674db940170
Dilong-D <dmarkowski19@gmail.com>
2017-11-24 01:18:41 +0100

    added classes for receivers, added building model, added multi-register send

---
+++ building.py
@@ -0,0 +1,96 @@
+import datetime
+
+
+# First Order ODE (y' = ub(x, y)) Solver using Euler method
+# xa: initial value of independent variable
+# xb: final value of independent variable
+# ya: initial value of dependent variable
+# n : number of steps (higher the better)
+# Returns value of y at xb.
+def Euler(f, xa, xb, ya, n):
+	h = (xb - xa) / float(n)
+	x = xa
+	y = ya
+	for i in range(n):
+		y += h * f(x, y)
+		x += h
+	return y
+
+
+def get_h(timestamp):
+	return datetime.datetime.fromtimestamp(timestamp).hour
+
+
+class Building:
+	def __init__(self):
+		# constants variable for simulation
+		self.M_H = 3000
+		self.C_H = 2700.0
+		self.K_H = 12000
+		self.F_MAX = 40 / 3600
+		self.M_B = 20000.0
+		self.C_B = 1000.0
+		self.K_E = 15000.0
+		self.C_W = 4200.0
+		self.RO = 1000.0
+		self.K_P = 0.0016  # wzmocnienie z simulinka
+		self.K_I = 6.8  # stala calkowania z simulinka
+
+		self.t_cob = 290.0
+		self.t_ro = 280.0
+		self.ub = 0.5
+		self.t_ref = 293  # temperatura referencyjan pomieszczenia (taka chcemy w budynku)
+		self.integral = 0  # calka do czlonu calkujacego regulatora
+		self.f_cob = self.ub * self.F_MAX
+		self.t_zco = 350
+		self.t_o = 277
+		self.t0 = 0
+		self.time = (datetime.datetime.fromtimestamp(0).strftime('%Y-%m-%d %H:%M:%S'))
+
+	def building_simulation_step(self, t0_arg, dt, t_zco_arg, t_o_arg):
+		self.time = (datetime.datetime.fromtimestamp(int(t0_arg)).strftime('%Y-%m-%d %H:%M:%S'))
+
+		hour = get_h(t0_arg)
+		if hour > 6 & hour < 21:
+			self.t_ref = 293.15
+		else:
+			self.t_ref = 288.15
+
+		self.t0 = t0_arg
+		t1 = self.t0 + dt
+
+		self.t_zco = t_zco_arg
+		self.t_o = t_o_arg
+		error = self.t_ref - self.t_ro  # obliczamy blad
+		self.integral += error  # zwiekszamy calke
+		if abs(error) < 0.1:  # jesli wynik jest dobry, wylaczamy czlon calkujacy
+			self.integral /= 100
+		if self.integral > 40:  # antiwindup
+			self.integral = 40
+		if self.integral < -40:  # antiwindup
+			self.integral = -40
+		self.ub = self.K_P * error + self.K_I * self.integral  # obliczanie sterowania
+		if self.ub > 1:
+			self.ub = 1
+		if self.ub < 0:
+			self.ub = 0
+
+		t_pco2 = Euler(lambda t, Tpc: self.ub * self.F_MAX * self.C_W * self.RO * (t_zco_arg - Tpc) / (
+			self.M_H * self.C_H) - self.K_H * (Tpc - self.t_ro) / (self.M_H * self.C_H), self.t0, t1, self.t_cob, 10)
+
+		t_r2 = Euler(lambda t, Tr: self.K_H * (self.t_cob - Tr) / (self.M_B * self.C_B) - self.K_E * (Tr - t_o_arg) / (
+			self.M_B * self.C_B), self.t0, t1, self.t_ro, 10)
+		self.t_cob = t_pco2
+		self.t_ro = t_r2
+		self.f_cob = self.ub * self.F_MAX
+
+	def __str__(self):
+		return "\nTime=" + self.time + " Temp. building=" + str(
+			self.t_ro) + " Temp. setpoint=" + str(
+			self.t_ref) + " Incoming H20 temp.=" + str(self.t_zco) + " Outcoming H20 temp.=" + str(
+			self.t_cob) + " Outcoming H20 flow=" + str(self.f_cob)
+
+# building = Building()
+# for i in range(0, 60 * 60 * 10):
+# 	building.building_simulation_step(i, 1, 400.0, 277)
+# 	print(building)
--- modbus_multimaster.py
+++ modbus_multimaster.py
@@ -7,6 +7,8 @@ from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
 from pymodbus.datastore import ModbusSparseDataBlock
 from pymodbus.server.sync import ModbusTcpServer
 
+from building import Building
+
 time_address = 0
 ready_flag_address = 0
 t_o_address = 100
@@ -19,145 +21,159 @@ CONTROLLER_IP = '192.168.1.115'
 LOGGER_IP = '192.168.1.115'
 OWN_IP = '192.168.1.116'
 PORT = 5555
+DT = 1
 
 
 # example function registered after as a time callback
 def step():
-    print('doing some stuff')
-    server.set_ready_flag()
-    print(server.get_ready_flag())
-    print(server.get_holding_register(time_address))
-    print(time_address)
-
-    logger.send_update()
-    water_flow_controller.send_update()
-    # set ready flag when you finish your job! it is really very important
+	# building.building_simulation_step(server.get_time(), DT, float(server.get_t_zco_from_registers())/100,
+	#  								  float(server.get_t_o_from_registers())/100)
+	building.building_simulation_step(server.get_time(), DT, 40000.0 / 100, 27300.0 / 100)
+	print("Sending updates")
+	# logger.send_update(building)
+	water_flow_controller.send_update(building)
+	print(building)
+	server.set_ready_flag()
 
 
 class Receiver(ModbusTcpClient):
-    def __init__(self, host, port, name):
-        super(Receiver, self).__init__(host, port)
-        self.name = name
-        self.initialization = True
+	def __init__(self, host, port, name):
+		super(Receiver, self).__init__(host, port)
+		self.name = name
+		self.initialization = True
 
-    def connected(self):
-        return self.socket
+	def connected(self):
+		return self.socket
 
-    def get_name(self):
-        return self.name
+	def get_name(self):
+		return self.name
 
 
 class WaterFlowController(Receiver):
-    def __init__(self):
-        super(WaterFlowController, self).__init__(CONTROLLER_IP, PORT, 'WaterFlowController')
+	def __init__(self):
+		super(WaterFlowController, self).__init__(CONTROLLER_IP, PORT, 'WaterFlowController')
 
-    def send_update(self):
-        super(WaterFlowController, self).write_register(t_cob_address, 420)
-        super(WaterFlowController, self).write_register(f_cob_address, 422)
+	def send_update(self, building_arg):
+		super(WaterFlowController, self).write_registers(t_cob_address, [int(building_arg.t_cob * 100), 0,
+																		 int(building_arg.f_cob * 1000000)])
+
+	# super(WaterFlowController, self).write_register(t_cob_address, int(building_arg.t_cob*100))
+	# super(WaterFlowController, self).write_register(f_cob_address, int(building_arg.f_cob*1000000))
 
 
 class Logger(Receiver):
-    def __init__(self):
-        super(Logger, self).__init__(LOGGER_IP, PORT, 'Logger')
+	def __init__(self):
+		super(Logger, self).__init__(LOGGER_IP, PORT, 'Logger')
 
-    def send_update(self):
-        super(Logger, self).write_register(t_cob_address, 420)
-        super(Logger, self).write_register(f_cob_address, 422)
-        super(Logger, self).write_register(ub_address, 424)
-        super(Logger, self).write_register(tr_address, 426)
+	def send_update(self, building_arg):
+		super(Logger, self).write_register(t_cob_address, int(building_arg.t_cob * 100))
+		super(Logger, self).write_register(f_cob_address, int(building_arg.f_cob * 1000000))
+		super(Logger, self).write_register(ub_address, int(building_arg.ub * 100))
+		super(Logger, self).write_register(tr_address, int(building_arg.t_ro * 100))
 
 
 class CoilsDataBlock(ModbusSparseDataBlock):
-    def __init__(self, values):
-        super(CoilsDataBlock, self).__init__(values)
-        self.time_flag_callback = None
+	def __init__(self, values):
+		super(CoilsDataBlock, self).__init__(values)
+		self.time_flag_callback = None
 
-    def setValues(self, address, values):
-        super(CoilsDataBlock, self).setValues(address, values)
-        if address == ready_flag_address:
-            if (super(CoilsDataBlock, self).getValues(address, 1)) == [False]:
-                _thread.start_new_thread(self.time_flag_callback, ())
+	def setValues(self, address, values):
+		super(CoilsDataBlock, self).setValues(address, values)
+		if address == ready_flag_address:
+			if (super(CoilsDataBlock, self).getValues(address, 1)) == [False]:
+				_thread.start_new_thread(self.time_flag_callback, ())
 
-    def set_time_flag_callback(self, function):
-        self.time_flag_callback = function
+	def set_time_flag_callback(self, function):
+		self.time_flag_callback = function
 
 
 class HoldingRegisterDataBlock(ModbusSparseDataBlock):
-    def __init__(self, values):
-        super(HoldingRegisterDataBlock, self).__init__(values)
-        self.timestamp = 0
-        self.time_callback = None
+	def __init__(self, values):
+		super(HoldingRegisterDataBlock, self).__init__(values)
+		self.timestamp = 0
+		self.time_callback = None
 
-    def setValues(self, address, values):
-        super(HoldingRegisterDataBlock, self).setValues(address, values)
-        if address == time_address:
-            self.timestamp = self.__calculate_timestamp__(values)
+	def setValues(self, address, values):
+		super(HoldingRegisterDataBlock, self).setValues(address, values)
+		if address == time_address:
+			self.timestamp = self.__calculate_timestamp__(values)
 
-    def get_time(self):
-        return self.timestamp
+	def get_time(self):
+		return self.timestamp
 
-    @staticmethod
-    def __calculate_timestamp__(register_values):
-        time1 = register_values[0]
-        time2 = register_values[1]
-        return (time1 << 16) | time2
+	@staticmethod
+	def __calculate_timestamp__(register_values):
+		time1 = register_values[0]
+		time2 = register_values[1]
+		return (time1 << 16) | time2
 
 
 class Server:
-    def __init__(self, address, port):
-        self.holding_register_block = HoldingRegisterDataBlock.create()
-        self.coil_block = CoilsDataBlock.create()
-        self.store = ModbusSlaveContext(hr=self.holding_register_block, co=self.coil_block, zero_mode=True)
-        self.context = ModbusServerContext(slaves=self.store, single=True)
-        self.server = ModbusTcpServer(self.context, address=(address, port))
-        self.thread = Thread(target=self.__run_thread__, args=())
+	def __init__(self, address, port):
+		self.holding_register_block = HoldingRegisterDataBlock.create()
+		self.coil_block = CoilsDataBlock.create()
+		self.store = ModbusSlaveContext(hr=self.holding_register_block, co=self.coil_block, zero_mode=True)
+		self.context = ModbusServerContext(slaves=self.store, single=True)
+		self.server = ModbusTcpServer(self.context, address=(address, port))
+		self.thread = Thread(target=self.__run_thread__, args=())
 
-    def __run_thread__(self):
-        self.server.serve_forever()
+	def __run_thread__(self):
+		self.server.serve_forever()
 
-    def run(self):
-        self.thread.start()
+	def run(self):
+		self.thread.start()
 
-    def stop(self):
-        self.server.server_close()
-        self.server.shutdown()
-        self.thread.join()
+	def stop(self):
+		self.server.server_close()
+		self.server.shutdown()
+		self.thread.join()
 
-    def get_holding_register(self, index):
-        return self.holding_register_block.getValues(index, 1)
+	def get_holding_register(self, index):
+		return self.holding_register_block.getValues(index, 1)
 
-    def set_time_flag_callback(self, function):
-        self.coil_block.set_time_flag_callback(function)
+	def set_time_flag_callback(self, function):
+		self.coil_block.set_time_flag_callback(function)
 
-    def set_ready_flag(self):
-        self.coil_block.setValues(ready_flag_address, True)
+	def set_ready_flag(self):
+		self.coil_block.setValues(ready_flag_address, True)
 
-    def get_ready_flag(self):
-        return self.coil_block.getValues(ready_flag_address, 1)
+	def get_ready_flag(self):
+		return self.coil_block.getValues(ready_flag_address, 1)
 
-    def get_time(self):
-        return self.holding_register_block.get_time()
+	def get_time(self):
+		return self.holding_register_block.get_time()
+
+	def get_t_o_from_registers(self):
+		return self.holding_register_block.getValues(t_o_address, 1)
+
+	def get_t_zco_from_registers(self):
+		return self.holding_register_block.getValues(t_zco_address, 1)
 
 
 if __name__ == '__main__':
-    # start new server
-    server = Server(OWN_IP, 5555)
-    # add a callback function which will be called with new timestamp each time new time is received
-    server.set_time_flag_callback(step)
-    # run the server
-    server.run()
-    print('Server is running')
+	# start new server
+	server = Server('', PORT)
+	# add a callback function which will be called with new timestamp each time new time is received
+	server.set_time_flag_callback(step)
+	# run the server
+	server.run()
+	print('Server is running')
 
-    water_flow_controller = WaterFlowController()
-    logger = Logger()
-    close = False
-    while not close:
-        try:
-            time.sleep(10)
-        except KeyboardInterrupt:
-            close = True
+	water_flow_controller = WaterFlowController()
+	logger = Logger()
+	building = Building()
+	close = False
+	while not close:
+		try:
+			time.sleep(10)
+			input_str = input()
+			if input_str == 'close':
+				close = True
+		except KeyboardInterrupt:
+			close = True
 
-    # remember to stop server while closing your application
-    server.stop()
+	print('Server is closing')
+	# remember to stop server while closing your application
+	server.stop()
 
-    print('finished!')
+	print('finished!')
